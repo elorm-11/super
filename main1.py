254a08b0c6cea099751aac694a3046411cf4311f
elorm-11 <elormkovey@gmail.com>
2024-12-23 19:43:32 +0000

    2 commit

---
+++ main1.py
@@ -0,0 +1,12 @@
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed
+        self.mileage = mileage
+
+class Bus(vehicle):
+    pass
+
+School_bus == Bus("School Volvo, 100, 12") 
+print("Vihicle Name:", School_bus.name, "speed:", School_bus.max_speed,"Mileage:", School_bus.milleage)                                 
